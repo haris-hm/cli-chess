4f52a76ec00788c5eb02dff5db9eda5637bd345c
DWF967 <42014844+DWF967@users.noreply.github.com>
2023-10-22 19:19:19 -0500

    Knight functionality

--- Knight.java
+++ Knight.java
@@ -16,8 +16,31 @@ public class Knight extends ChessPiece {
 
     @Override
     public ArrayList<int[]> calculateValidBoardMoves(Board currentBoard) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calculateValidBoardMoves'");
+        ArrayList<int[]> possibleMoves = new ArrayList<int[]>();
+        ChessPiece[][] activePieces = new ChessPiece[9][9];
+
+        int[][] validMoves = {{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
+
+        for (ChessPiece piece : currentBoard.getActivePieces()) {
+            activePieces[piece.getRankPosition()][piece.getFilePosition()] = piece;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            int[] moveCoord = {rankPosition + validMoves[i][0], filePosition + validMoves[i][1]};
+
+            if (moveCoord[0] <= 8 && moveCoord[0] >= 1 && moveCoord[1] <= 8 && moveCoord[1] >= 1) {
+                if (activePieces[moveCoord[0]][moveCoord[1]] != null && activePieces[moveCoord[0]][moveCoord[1]].isBlack() != this.isBlack()) {
+                    possibleMoves.add(moveCoord);
+                }
+                else if (activePieces[moveCoord[0]][moveCoord[1]] != null && activePieces[moveCoord[0]][moveCoord[1]].isBlack() == this.isBlack()){
+                    continue;
+                }
+
+                possibleMoves.add(moveCoord);
+            }
+        }
+
+        return possibleMoves;
     }
     
 }
